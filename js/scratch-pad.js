a864aefed438e3b86a1d7a6859e315ea7ce2bae1
Alaa Moustapha <alaamoustapha21@gmail.com>
2020-07-27 00:13:46 +0200

    upload scratch pad plugin to fix undo and redo

--- js/scratch-pad.js
+++ js/scratch-pad.js
@@ -3,6 +3,16 @@ $('#scratch_pad').click(function () {
     $("#scratch-pad-dialog").dialog({
         width: 600,
         autoOpen: true,
+        
     }).prev(".ui-dialog-titlebar").addClass("pencil_icon");
     $("#scratch-pad-dialog").css('display', 'block');
+    tinymce.init({
+        // General options
+        selector : "#scratch-pad-textarea",
+        toolbar: ' copy | cut | paste | undo | redo ',
+        menubar: false,
+        statusbar: false,
+        
+    });
+      
 });
