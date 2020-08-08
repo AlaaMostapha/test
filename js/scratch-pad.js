99304ce7a2777c017face63514bcbbb365c5b971
Alaa Moustapha <alaamoustapha21@gmail.com>
2020-08-08 22:54:39 +0200

    make no edit in tinymce

--- js/scratch-pad.js
+++ js/scratch-pad.js
@@ -1,3 +1,9 @@
+function catchSelected() {
+    var range = document.createRange();
+    range.selectNodeContents(document.getElementById('scratch-pad-textarea'));
+    var selection = window.getSelection();
+    selection.addRange(range);
+}
 //on click(scratch pad) show it's dialog
 $('#scratch_pad').click(function () {
     $("#scratch-pad-dialog").dialog({
@@ -9,10 +15,30 @@ $('#scratch_pad').click(function () {
     tinymce.init({
         // General options
         selector : "#scratch-pad-textarea",
-        toolbar: ' copy | cut | paste | undo | redo ',
+        toolbar: ' copy | cut | paste |pastetext | undo | redo ',
         menubar: false,
         statusbar: false,
-        content_css: "/css/scratchpad.css"
+        // content_css: "/css/scratchpad.css",
+        setup: function(editor) {
+           
+            editor.on('init', function(e) {
+              console.log('The Editor has initialized.');
+            });
+            editor.on('paste', function(e) {
+                console.log('The Editor has paste.');
+                const text =  navigator.clipboard.readText();
+                console.log(text)
+                document.getElementById("scratch-pad-textarea").value += text;
+            });
+            editor.on('copy', function(e) {
+                console.log('The Editor has copy.');
+                // catch selected
+    catchSelected();
+    // Copy
+    document.execCommand("copy");
+            });
+          }
+          
     });
       
 });
