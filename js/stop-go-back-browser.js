84437314cd6cdd519bacb8175c1a269fa254ae9e
Alaa Moustapha <alaamoustapha21@gmail.com>
2020-08-09 13:49:26 +0200

    add new senario for goback

--- js/stop-go-back-browser.js
+++ js/stop-go-back-browser.js
@@ -1,5 +1,19 @@
-history.pushState(null, null, location.href);
-window.onpopstate = function () {
-    history.go(1);
-};
+// history.pushState(null, null, location.href);
+// window.onpopstate = function () {
+//     history.go(1);
+// };
     
+// window.history.forward(); 
+//         function noBack() { 
+//             window.history.forward(); 
+//         } 
+
+// function preventBack() { window.history.forward(); }
+//         setTimeout("preventBack()", 0);
+//         window.onunload = function () { null };
+
+
+
+    
+window.history.forward();
+	function noBack() { window.history.forward(); }
